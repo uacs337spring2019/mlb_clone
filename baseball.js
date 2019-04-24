9303ba6206c99ea25b578731556a81fdd1b1a72b
Benjamin Noriega <35078538+noriegab1@users.noreply.github.com>
2019-04-24 14:09:09 -0700

    Add files via upload

---
+++ baseball.js
@@ -0,0 +1,151 @@
+"use strict";
+
+(function() {
+
+	window.onload = function() {
+		let standingButton = document.getElementById("standButton");
+		standingButton.onclick = pullStandings;
+	};
+
+	function pullStandings() {
+		let url = 'https://api.mysportsfeeds.com/v2.1/pull/mlb/2019-regular/standings.json';
+		let apikey_token = '';
+
+		let h = new Headers();
+		h.append("Authorization", "Basic " + btoa("8cb8bd89-32ce-446d-b5f6-714475" + ":" + "MYSPORTSFEEDS"));
+
+		let req = new Request(url, {
+			method: 'GET',
+			headers: h,
+
+		});
+
+		fetch(req)
+			.then(checkStatus)
+			.then(function(responseText){
+				let json = JSON.parse(responseText);
+
+				displayStandings(json);
+
+			})
+			.catch(function(error){
+
+			});
+	}
+
+	function displayStandings(data) {
+
+		let standingDiv = document.getElementById("standings");
+		standings.style.display = "block";
+		let standButton = document.getElementById("standButton");
+		standButton.style.display = "none";
+
+		let standingTable = document.getElementById("standingtable")
+		standingTable.innerHTML = "";
+
+
+		let dbacks = data["teams"][0];
+		let rockies = data["teams"][8];
+		let dodgers = data["teams"][13];
+		let padres = data["teams"][22];
+		let giants = data["teams"][23];
+
+		let division = {};
+
+		division[dbacks["divisionRank"]["rank"]] = dbacks;
+		division[rockies["divisionRank"]["rank"]] = rockies;
+		division[dodgers["divisionRank"]["rank"]] = dodgers;
+		division[padres["divisionRank"]["rank"]] = padres;
+		division[giants["divisionRank"]["rank"]] = giants;
+
+		let legend = document.createElement("tr");
+		let rankL = document.createElement("th");
+		rankL.innerHTML += "Rank";
+		let nameL = document.createElement("th");
+		nameL.innerHTML += "Team";
+		let recordL = document.createElement("th");
+		recordL.innerHTML += "W - L";
+		legend.appendChild(rankL);
+		legend.appendChild(nameL);
+		legend.appendChild(recordL);
+		standingTable.appendChild(legend);
+
+		let row1 = document.createElement("tr");
+		let rank1 = document.createElement("td");
+		rank1.innerHTML += 1;
+		let name1 = document.createElement("td");
+		name1.innerHTML += division[1]["team"]["city"] + " " + division[1]["team"]["name"];
+		let record1 = document.createElement("td");
+		record1.innerHTML = division[1]["stats"]["standings"]["wins"] + " - ";
+		record1.innerHTML += division[1]["stats"]["standings"]["losses"];
+		row1.appendChild(rank1);
+		row1.appendChild(name1);
+		row1.appendChild(record1);
+		standingTable.appendChild(row1);
+
+		let row2 = document.createElement("tr");
+		let rank2 = document.createElement("td");
+		rank2.innerHTML += 2;
+		let name2 = document.createElement("td");
+		name2.innerHTML += division[2]["team"]["city"] + " " + division[2]["team"]["name"];
+		let record2 = document.createElement("td");
+		record2.innerHTML = division[2]["stats"]["standings"]["wins"] + " - ";
+		record2.innerHTML += division[2]["stats"]["standings"]["losses"];
+		row2.appendChild(rank2);
+		row2.appendChild(name2);
+		row2.appendChild(record2);
+		standingTable.appendChild(row2);
+
+		let row3 = document.createElement("tr");
+		let rank3 = document.createElement("td");
+		rank3.innerHTML += 3;
+		let name3 = document.createElement("td");
+		name3.innerHTML += division[3]["team"]["city"] + " " + division[3]["team"]["name"];
+		let record3 = document.createElement("td");
+		record3.innerHTML = division[3]["stats"]["standings"]["wins"] + " - ";
+		record3.innerHTML += division[3]["stats"]["standings"]["losses"];
+		row3.appendChild(rank3);
+		row3.appendChild(name3);
+		row3.appendChild(record3);
+		standingTable.appendChild(row3);
+
+		let row4 = document.createElement("tr");
+		let rank4 = document.createElement("td");
+		rank4.innerHTML += 4;
+		let name4 = document.createElement("td");
+		name4.innerHTML += division[4]["team"]["city"] + " " + division[4]["team"]["name"];
+		let record4 = document.createElement("td");
+		record4.innerHTML = division[4]["stats"]["standings"]["wins"] + " - ";
+		record4.innerHTML += division[4]["stats"]["standings"]["losses"];
+		row4.appendChild(rank4);
+		row4.appendChild(name4);
+		row4.appendChild(record4);
+		standingTable.appendChild(row4);
+
+		let row5 = document.createElement("tr");
+		let rank5 = document.createElement("td");
+		rank5.innerHTML += 5;
+		let name5 = document.createElement("td");
+		name5.innerHTML += division[5]["team"]["city"] + " " + division[5]["team"]["name"];
+		let record5 = document.createElement("td");
+		record5.innerHTML = division[5]["stats"]["standings"]["wins"] + " - ";
+		record5.innerHTML += division[5]["stats"]["standings"]["losses"];
+		row5.appendChild(rank5);
+		row5.appendChild(name5);
+		row5.appendChild(record5);
+		standingTable.appendChild(row5);
+
+
+		console.log(division);
+	}
+
+
+	function checkStatus(response) {
+		if(response.status >= 200 && response.status < 300) {
+			return response.text();
+		} else {
+			return Promise.reject(new Error(response.status + ": " + response.statusText));
+		}
+	}
+
+})();
---
+++ dbacks.js
@@ -0,0 +1,120 @@
+
+
+"use strict";
+
+(function() {
+
+	window.onload = pullRecord;
+
+	function pullRecord() {
+		let url = 'https://api.mysportsfeeds.com/v2.1/pull/mlb/2019-regular/team_stats_totals.json?team=ari';
+		let apikey_token = '';
+
+		let h = new Headers();
+		h.append("Authorization", "Basic " + btoa("8cb8bd89-32ce-446d-b5f6-714475" + ":" + "MYSPORTSFEEDS"));
+
+		let req = new Request(url, {
+			method: 'GET',
+			headers: h,
+
+		});
+
+		fetch(req)
+			.then(checkStatus)
+			.then(function(responseText){
+				let json = JSON.parse(responseText);
+
+				displayRecord(json);
+				teamStats(json);
+
+			})
+			.catch(function(error){
+
+			});
+	}
+
+	function displayRecord(data){
+		let recordDiv = document.getElementById("record");
+
+		let team = data["teamStatsTotals"][0];
+		let wins = team["stats"]["standings"]["wins"]
+		let loss = team["stats"]["standings"]["losses"]
+
+		let recordStatement = document.createElement("p");
+
+		recordStatement.innerHTML += wins + " - " + loss + "<br />";
+
+		let runsF = team["stats"]["standings"]["runsFor"];
+		let runsA= team["stats"]["standings"]["runsAgainst"];
+
+		recordStatement.innerHTML += "<hr />Runs Scored: " + runsF + "<br />";
+		recordStatement.innerHTML += "Runs Allowed: " + runsA + "<br />";
+
+		recordDiv.appendChild(recordStatement);
+
+	}
+
+	function teamStats(data) {
+		let stats = data["teamStatsTotals"][0]["stats"];
+		console.log(stats);
+		
+		let batStats = document.getElementById("batstats");
+
+		let batting = stats["batting"];
+
+		let avg = batting["battingAvg"];
+		let homers = batting["homeruns"];
+		let rbi = batting["runsBattedIn"];
+		let obp = batting["batterOnBasePct"];
+		let slg = batting["batterSluggingPct"];
+		let ops = batting["batterOnBasePlusSluggingPct"];
+		let sb = batting["stolenBases"];
+
+		batStats.innerHTML = "AVG: " + avg + "<br />";
+		batStats.innerHTML += "HR: " + homers + "<br />";
+		batStats.innerHTML += "RBI: " + rbi + "<br />";
+		batStats.innerHTML += "OBP: " + obp + "<br />";
+		batStats.innerHTML += "SLG: " + slg + "<br />";
+		batStats.innerHTML += "OPS: " + ops + "<br />";
+		batStats.innerHTML += "Stolen Bases: " + sb + "<br />";
+
+		let pitchStats = document.getElementById("pitchstats");
+
+		let pitching = stats["pitching"];
+
+		let era = pitching["earnedRunAvg"];
+		let strikeouts = pitching["pitcherStrikeouts"];
+		let pitchesThrown = pitching["pitchesThrown"];
+		let whip = pitching["walksAndHitsPerInningPitched"];
+		let ppi = pitching["pitchesPerInning"];
+
+		pitchStats.innerHTML = "ERA: " + era + "<br />";
+		pitchStats.innerHTML += "Strikeouts: " + strikeouts + "<br />";
+		pitchStats.innerHTML += "Pitches Thrown: " + pitchesThrown + "<br />";
+		pitchStats.innerHTML += "WHIP: " + whip + "<br />";
+		pitchStats.innerHTML += "Pitches Per Inning: " + ppi + "<br />";
+
+		let fieldStats = document.getElementById("fieldstats");
+
+		let fielding = stats["fielding"];
+
+		let fpct = fielding["fieldingPct"];
+		let errors = fielding["errors"];
+		let assists = fielding["assists"];
+
+		fieldStats.innerHTML = "Fielding PCT: " + fpct + "%<br />";
+		fieldStats.innerHTML += "Errors: " + errors + "<br />";
+		fieldStats.innerHTML += "Assists: " + assists + "<br />";
+
+	}
+
+
+	function checkStatus(response) {
+		if(response.status >= 200 && response.status < 300) {
+			return response.text();
+		} else {
+			return Promise.reject(new Error(response.status + ": " + response.statusText));
+		}
+	}
+
+})();
